c73a34924cc508921527add982dafe2c02e74f38
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-04-07 00:49:27 -0400

    Added BRAKER2 2.1.5

--- 2.1.5/generate_binaries.sh
+++ 2.1.5/generate_binaries.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 
-TOOLS=(aa2nonred.pl                exoniphyDb2hints.pl         gth2gtf.pl                  pslMap.pl
-add_name_to_gff3.pl         export2sam.pl               hal2maf_split.pl            randomSplit.pl
-align2hints.pl              extractTranscriptEnds.pl    interpolate_sam.pl          retroDB2hints.pl
-augustus2browser.pl         filter-ppx.pl               joinPeptides.pl             rmRedundantHints.pl
-augustus2gbrowse.pl         filterGenemark.pl           join_aug_pred.pl            runAllSim4.pl
-autoAug.pl                  filterGenes.pl              join_mult_hints.pl          sam2vcf.pl
-autoAugPred.pl              filterGenesIn.pl            legacy_blast.pl             samMap.pl
-autoAugTrain.pl             filterGenesIn_mRNAname.pl   log_reg_prothints.pl        samtools.pl
-bedgraph2wig.pl             filterGenesOut_mRNAname.pl  maf2conswig.pl              scipiogff2gff.pl
-blast2sam.pl                filterInFrameStopCodons.pl  makblk.pl                   seq_cache_populate.pl
-blat2gbrowse.pl             filterIntronsFindStrand.pl  makeMatchLists.pl           setStopCodonFreqs.pl
-blat2hints.pl               filterMaf.pl                makeUtrTrainingSet.pl       setup-deps.pl
-block2prfl.pl               filterPSL.pl                makeidx.pl                  simpleFastaHeaders.pl
-bowtie2sam.pl               filterShrimp.pl             maskNregions.pl             simplifyFastaHeaders.pl
-braker.pl                   filterSpliceHints.pl        merge_masking.pl            soap2sam.pl
-braker_cleanup.pl           filter_augustus_gff.pl      merge_transcript_sets.pl    splitMfasta.pl
-catchr.pl                   findGenesInIntrons.pl       moveParameters.pl           split_wiggle.pl
-cegma2gff.pl                findGffNamesInFasta.pl      msa2prfl.pl                 spspaln.pl
-checkParamArchive.pl        fix_joingenes_gtf.pl        new_species.pl              startAlign.pl
-cleanDOSfasta.pl            gatech_pmp2hints.pl         novo2sam.pl                 summarizeACGTcontent.pl
-clusterAndSplitGenes.pl     gbSmallDNA2gff.pl           opt_init_and_term_probs.pl  transMap2hints.pl
-compare_masking.pl          gbrowseold2gff3.pl          optimize_augustus.pl        uniquePeptides.pl
-computeFlankingRegion.pl    getAnnoFasta.pl             parseSim4Output.pl          update_blastdb.pl
-createAugustusJoblist.pl    getLinesMatching.pl         partition_gtf2gb.pl         utrgff2gbrowse.pl
-del_from_prfl.pl            get_loci_from_gb.pl         pasapolyA2hints.pl          weedMaf.pl
-downsample_traingenes.pl    gff2gbSmallDNA.pl           peptides2alternatives.pl    wgsim_eval.pl
-edirect.pl                  gffGetmRNA.pl               peptides2hints.pl           wig2hints.pl
-evalCGP.pl                  gp2othergp.pl               phastconsDB2hints.pl        wigchoose.pl
-eval_dualdecomp.pl          gtf2aa.pl                   polyA2hints.pl              writeResultsPage.pl
-eval_multi_gtf.pl           gtf2bed.pl                  prints2prfl.pl              yaml2gff.1.4.pl
-exonerate2hints.pl          gtf2gff.pl                  psl2sam.pl                  zoom2sam.pl)
+TOOLS=(aa2nonred.pl		    filterMaf.pl		optimize_augustus.pl
+add_name_to_gff3.pl	    filterPSL.pl		parseSim4Output.pl
+align2hints.pl		    filterShrimp.pl		partition_gtf2gb.pl
+augustus2browser.pl	    filterSpliceHints.pl	pasapolyA2hints.pl
+augustus2gbrowse.pl	    filter_augustus_gff.pl	peptides2alternatives.pl
+autoAug.pl		    findGenesInIntrons.pl	peptides2hints.pl
+autoAugPred.pl		    findGffNamesInFasta.pl	phastconsDB2hints.pl
+autoAugTrain.pl		    fix_joingenes_gtf.pl	polyA2hints.pl
+bedgraph2wig.pl		    gatech_pmp2hints.pl		prints2prfl.pl
+blast2sam.pl		    gbSmallDNA2gff.pl		psl2sam.pl
+blat2gbrowse.pl		    gbrowseold2gff3.pl		pslMap.pl
+blat2hints.pl		    getAnnoFasta.pl		randomSplit.pl
+block2prfl.pl		    getLinesMatching.pl		retroDB2hints.pl
+bowtie2sam.pl		    get_loci_from_gb.pl		rmRedundantHints.pl
+braker.pl		    gff2gbSmallDNA.pl		runAllSim4.pl
+braker_cleanup.pl	    gffGetmRNA.pl		sam2vcf.pl
+catchr.pl		    gp2othergp.pl		samMap.pl
+cegma2gff.pl		    gtf2aa.pl			samtools.pl
+checkParamArchive.pl	    gtf2bed.pl			scipiogff2gff.pl
+cleanDOSfasta.pl	    gtf2gff.pl			seq_cache_populate.pl
+clusterAndSplitGenes.pl     gth2gtf.pl			setStopCodonFreqs.pl
+compare_masking.pl	    hal2maf_split.pl		simpleFastaHeaders.pl
+computeFlankingRegion.pl    interpolate_sam.pl		simplifyFastaHeaders.pl
+createAugustusJoblist.pl    joinPeptides.pl		soap2sam.pl
+del_from_prfl.pl	    join_aug_pred.pl		splitMfasta.pl
+downsample_traingenes.pl    join_mult_hints.pl		split_wiggle.pl
+evalCGP.pl		    legacy_blast.pl		spspaln.pl
+eval_dualdecomp.pl	    log_reg_prothints.pl	startAlign.pl
+eval_multi_gtf.pl	    maf2conswig.pl		summarizeACGTcontent.pl
+exonerate2hints.pl	    makblk.pl			transMap2hints.pl
+exoniphyDb2hints.pl	    makeMatchLists.pl		uniquePeptides.pl
+export2sam.pl		    makeUtrTrainingSet.pl	update_blastdb.pl
+extractTranscriptEnds.pl    makeidx.pl			utrgff2gbrowse.pl
+filter-ppx.pl		    maskNregions.pl		weedMaf.pl
+filterGenemark.pl	    merge_masking.pl		wgsim_eval.pl
+filterGenes.pl		    merge_transcript_sets.pl	wig2hints.pl
+filterGenesIn.pl	    moveParameters.pl		wigchoose.pl
+filterGenesIn_mRNAname.pl   msa2prfl.pl			writeResultsPage.pl
+filterGenesOut_mRNAname.pl  new_species.pl		yaml2gff.1.4.pl
+filterInFrameStopCodons.pl  novo2sam.pl			zoom2sam.pl
+filterIntronsFindStrand.pl  opt_init_and_term_probs.pl)
 
 cat << EOF > template
 #!/bin/bash
@@ -49,7 +59,7 @@ if [ -d /local ]; then
 	OPTIONS=\$OPTIONS" -B /local"
 fi
 
-singularity exec \$OPTIONS \$DIRECTORY/singularity-\$PACKAGE-\$VERSION.sif \$TOOL "\$@"
+singularity exec \$OPTIONS \$DIRECTORY/singularity-\$PACKAGE-\$VERSION.sif /opt/conda/bin/\$TOOL "\$@"
 EOF
 
 for TOOL in "${TOOLS[@]}"
